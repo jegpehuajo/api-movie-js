b7d0e07cb5d7d18d565278ef23c771c1763dbd8c
Jorge E Gomez <jeg1986@gmail.com>
2023-09-10 18:51:21 -0300

    Muestro el poster.

--- src/main.js
+++ src/main.js
@@ -9,7 +9,23 @@
     const response = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await response.json();
 
-    // const movies = data.results;
+    const movies = data.results;
+    movies.forEach(movie => {
+      const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+      const movieContainer = document.createElement('div');
+      movieContainer.classList.add('movie-container');
+
+      const movieImg = document.createElement('img');
+      movieImg.classList.add('movie-img');
+      movieImg.setAttribute('alt',movie.title);
+      movieImg.setAttribute(
+        'src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+      );
+      
+      movieContainer.appendChild(movieImg);
+      trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
     console.log({data});
   }
 
